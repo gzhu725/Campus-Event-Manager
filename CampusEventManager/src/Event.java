9147b95c07a632376040e4a97b108c40182b9f37
gzhu725 <gloriazhu66@gmail.com>
2025-05-09 20:34:58 -0400

    refactor event

--- CampusEventManager/src/Event.java
+++ CampusEventManager/src/Event.java
@@ -1,75 +1,86 @@
 import java.util.*;
 
 public class Event {
-  private String name;
-  private String date;
-  private String location;
-  private String time;
-  private String description;
-  private Organizer organizer;
-  private List<Student> attendees;
+    private String name;
+    private String date;
+    private String location;
+    private String time;
+    private String description;
+    private Organizer organizer;
+    private List<Student> attendees;
 
-  public Event(String name, String date, String location, String time, String description, Organizer organizer) {
-    this.name = name;
-    this.date = date;
-    this.location = location;
-    this.time = time;
-    this.description = description;
-    this.organizer = organizer;
-  }
-
-  public Organizer getOrganizer() {
-    return organizer;
-  }
-  
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public String getDate() {
-    return date;
-  }
-  public void setDate(String date) {
-    this.date = date;
-  }
-  public String getLocation() {
-    return location;
-  }
-  public void setLocation(String location) {
-    this.location = location;
-  }
-  public String getTime() {
-    return time;
-  }
-  public void setTime(String time) {
-    this.time = time;
-  }
-  public String getDescription() {
-    return description;
-  }
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public void addAttendee(Student student) {
-    if (attendees == null) {
-      attendees = new ArrayList<>();
+    public Event(String name, String date, String location, String time, String description, Organizer organizer) {
+        this.name = name;
+        this.date = date;
+        this.location = location;
+        this.time = time;
+        this.description = description;
+        this.organizer = organizer;
+        this.attendees = new ArrayList<>(); 
     }
-    attendees.add(student);
-  }
 
-  public void removeAttendee(Student student) {
-    if (attendees != null) {
-      attendees.remove(student);
+
+    //getters and setters
+
+    public Organizer getOrganizer() {
+        return organizer;
     }
-  }
 
-  public List<Student> getAttendees() {
-    return attendees;
-  }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  
-  
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    // add and remove student from events
+    public void addAttendee(Student student) {
+        if (!attendees.contains(student)) {
+            attendees.add(student);
+        }
+    }
+
+    public void removeAttendee(Student student) {
+        attendees.remove(student);
+    }
+
+
+    //get all students who participating in the event
+    public List<Student> getAttendeeList() {
+        return attendees;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + date + " @ " + time + ")";
+    }
 }
