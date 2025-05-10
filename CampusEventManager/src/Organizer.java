d42ea0ba7f0da565ffdf9b584bec435083c4161c
Savina Chan <savinachan1582@gmail.com>
2025-05-09 21:02:33 -0400

    refactor Organizer class

--- CampusEventManager/src/Organizer.java
+++ CampusEventManager/src/Organizer.java
@@ -18,53 +18,46 @@ public class Organizer {
         this.createdEvents = new ArrayList<>(); // Initialize the event list
     }
 
-    // Creates a new event and add it to the organizer's list
+    // Adds a new event to the organizer's list
     public void createEvent(Event event) {
         createdEvents.add(event);
     }
 
-    // // Edits an event's details by finding it by its name
-    // public void editEvent(String currentEventName, String newEventName, String newDate, String newLocation) {
-    //     for (Event event : createdEvents) {
-    //         if (event.getName().equals(currentEventName)) {
-    //             event.setName(newEventName);
-    //             event.setDate(newDate);
-    //             event.setLocation(newLocation);
-    //             return;
-    //         }
-    //     }
-    // }
-
-    // Getters and setters for all attributes
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public String getUsername() {
         return username;
     }
-    
+
     public void setUsername(String username) {
         this.username = username;
     }
-    
+
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public List<Event> getCreatedEvents() {
         return createdEvents;
     }
-    
+
     public void setCreatedEvents(List<Event> createdEvents) {
         this.createdEvents = createdEvents;
-    }    
+    }
+
+    // Used for displaying in lists/logging
+    @Override
+    public String toString() {
+        return name + " (" + username + ")";
+    }
 }
